b39f7e2ffbd9c27c55179f162043af324b9dc6af
flazepe <51487578+flazepe@users.noreply.github.com>
2020-04-28 09:57:42 +0800

    Create upload.php

---
+++ upload.php
@@ -0,0 +1,25 @@
+<?php
+// Configuration
+$key = "your key";
+$dir = "your file directory name";
+
+isset($_SERVER["HTTPS"]) ? $protocol = "https" : $protocol = "http";
+if ($_SERVER["REQUEST_URI"] == "/robot.txt") die("User-Agent: *\nDisallow: /");
+
+if (!is_dir("i")) mkdir("i", 0755);
+
+if (isset($_POST["key"])) {
+    if ($_POST["key"] == $key) {
+        $parts = explode(".", $_FILES["d"]["name"]);
+        $target = getcwd() . "/i/" . $_POST["name"] . "." . end($parts);
+        
+        if (move_uploaded_file($_FILES["d"]["tmp_name"], $target)) {
+            $target_parts = explode("/i/", $target);
+            echo "$protocol://" . $_SERVER["HTTP_HOST"] . "/i/" . end($target_parts);
+        }
+        
+        else echo "Sorry, there was a problem uploading your file. (Ensure your directory has 777 permissions)";
+    }
+}
+
+else header("HTTP/1.0 404 Not Found");
