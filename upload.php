9a16e80717359f13e6514f744877a26b86f2b194
flazepe <51487578+flazepe@users.noreply.github.com>
2020-04-28 10:03:50 +0800

    Update upload.php

--- upload.php
+++ upload.php
@@ -11,11 +11,11 @@ if (!is_dir(dir)) mkdir(dir, 0755);
 if (isset($_POST["key"])) {
     if ($_POST["key"] == $key) {
         $parts = explode(".", $_FILES["d"]["name"]);
-        $target = getcwd() . "/i/" . $_POST["name"] . "." . end($parts);
+        $target = getcwd() . "/$dir/" . $_POST["name"] . "." . end($parts);
         
         if (move_uploaded_file($_FILES["d"]["tmp_name"], $target)) {
             $target_parts = explode("/i/", $target);
-            echo "$protocol://" . $_SERVER["HTTP_HOST"] . "/i/" . end($target_parts);
+            echo "$protocol://" . $_SERVER["HTTP_HOST"] . "/$dir/" . end($target_parts);
         }
         
         else echo "Sorry, there was a problem uploading your file. (Ensure your directory has 777 permissions)";
